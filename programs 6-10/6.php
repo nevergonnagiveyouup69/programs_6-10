8169f520554db9fa7946cba5bb05b3fcf22b71e0
nevergonnagiveyouup69 <97106120+nevergonnagiveyouup69@users.noreply.github.com>
2022-08-04 10:41:04 +0530

    Update 6.php

--- programs 6-10/6.php
+++ programs 6-10/6.php
@@ -3,7 +3,7 @@ $f=fopen("count.txt","r+");
 $line=fgets($f);
 fseek($f,0);
 $line++;
-echo "Number of times visited the website is : ".$line;
+echo "Number_of_times visited the website is : ".$line;
 fwrite($f,$line);
 fclose($f);
 ?>
